80e98b64d19e4a33c71bdb2e2f696cfa99237f92
luciantin <lord66tin66@gmail.com>
2025-06-29 08:56:45 +0000

    Changed telemetry to Core package

--- cmd/config/handle.go
+++ cmd/config/handle.go
@@ -3,34 +3,28 @@ package function
 import (
 	"context"
 	"fmt"
+	"github.com/CoreKitMDK/corekit-service-core/v2/pkg/core"
 	"github.com/CoreKitMDK/corekit-service-logger/v2/pkg/logger"
-	"net/http"
-	//"github.com/CoreKitMDK/corekit-service-logger/v2/pkg/logger"
-	"github.com/CoreKitMDK/corekit-service-telemetry/v2/pkg/telemetry"
 	"github.com/go-redis/redis/v8"
+	"net/http"
 )
 
-//var config_config_json = "{\"use_console\":true,\"use_nats\":true,\"nats_url\":\"internal-logger-broker-nats-client\",\"nats_username\":\"internal-logger-broker\",\"nats_password\":\"internal-logger-broker\"}"
-//
-//var Logger_config, _	 = logger.FromJsonString(config_config_json)
-//var Logger = Logger_config.Init()
-
 var (
-	Telemetry, _ = telemetry.NewTelemetry()
-	ctx          = context.Background()
-	rdb          = redis.NewClient(&redis.Options{
+	Core, _ = core.NewCore()
+	ctx     = context.Background()
+	rdb     = redis.NewClient(&redis.Options{
 		Addr: "internal-configuration-kvstore-master:6379",
 	})
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
 
-	trace := Telemetry.Tracing.TraceHttpRequest(r).Start()
+	trace := Core.Tracing.TraceHttpRequest(r).Start()
 
 	key := r.URL.Query().Get("key")
 	if key == "" {
 		caller := r.Header.Get("Caller")
-		Telemetry.Logger.Log(logger.ERROR, "Missing key parameter in query string for caller : "+caller)
+		Core.Logger.Log(logger.ERROR, "Missing key parameter in query string for caller : "+caller)
 		http.Error(w, "missing key parameter in query string", http.StatusBadRequest)
 		return
 	}
@@ -38,12 +32,12 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	val, err := rdb.Get(ctx, key).Result()
 	if err == redis.Nil {
 		caller := r.Header.Get("Caller")
-		Telemetry.Logger.Log(logger.ERROR, "Key not found : "+key+" for caller : "+caller)
+		Core.Logger.Log(logger.ERROR, "Key not found : "+key+" for caller : "+caller)
 		http.Error(w, "key not found", http.StatusNotFound)
 		return
 	} else if err != nil {
 		caller := r.Header.Get("Caller")
-		Telemetry.Logger.Log(logger.ERROR, "KV connection error : "+err.Error()+" for caller : "+caller)
+		Core.Logger.Log(logger.ERROR, "KV connection error : "+err.Error()+" for caller : "+caller)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -51,7 +45,7 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", val)
 
 	caller := r.Header.Get("Caller")
-	Telemetry.Logger.Log(logger.DEBUG, "Key found :) : "+key+" for caller : "+caller)
+	Core.Logger.Log(logger.DEBUG, "Key found :) : "+key+" for caller : "+caller)
 
 	trace.TraceHttpResponseWriter(w).End()
 }
